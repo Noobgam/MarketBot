11732541c8ddaff58635bf520d449ae7c618ff77
noobgam <noobgam@NOOBGAM-W10>
2017-08-08 16:50:55 +0300

    Implemented MarketLogger (was easy, wasn't it, huh?)

---
+++ SteamBot/MarketLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    class MarketLogger : SteamBot.Log
+    {
+        MarketLogger(String path) : base(path, "Market")
+        {
+        }
+    }
+}
