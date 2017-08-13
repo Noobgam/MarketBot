e1c372b89b54846d13a14e9274b5bfb306cd1c39
noobgam <noobgam@NOOBGAM-W10>
2017-08-13 14:48:13 +0300

    First attempt to merge them both

---
+++ MarketBot/DotaLogic.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using SteamBot.SteamGroups;
+using SteamKit2;
+using WebSocket4Net;
+using SteamTrade;
+using SteamKit2.Internal;
+using SteamTrade.TradeOffer;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace NDota2Market
+{
+
+    public class Logic
+    {
+        public Logic()
+        {
+            LoadDataBase();
+            Thread starter = new Thread(new ThreadStart(StartUp));
+            starter.Start();           
+        }
+
+        private void StartUp()
+        {
+            while (Protocol == null)
+            {
+                Thread.Sleep(10);
+            }
+
+            Thread saver = new Thread(new ThreadStart(SaveDataBaseCycle));
+            saver.Start();
+            Thread seller = new Thread(new ThreadStart(SellFromQueue));
+            seller.Start();
+            Thread adder = new Thread(new ThreadStart(AddNewItems));
+            adder.Start();
+            Thread setter = new Thread(new ThreadStart(SetNewOrder));
+            setter.Start();
+
+        }
+
+        void SetNewOrder()
+        {
+            while (true)
+            {
+                if (needOrder.Count != 0)
+                {
+                    HistoryItem item = needOrder.Dequeue();
+                    try
+                    {
+                        int price = Protocol.getBestOrder(item.i_classid, item.i_instanceid);
+                        Thread.Sleep(3000);
+
+                        SalesHistory history = dataBase[item.i_market_name];
+                        Console.WriteLine("Checking item..." + price + "  vs  " + history.median);
+                        if (price < 30000 && history.median * 0.8 > price && history.median * 0.8 - price > 30)
+                        {
+                            try
+                            {
+                                Protocol.SetOrder(item.i_classid, item.i_instanceid, ++price);
+                                Console.WriteLine("Settled order for " + item.i_market_name);
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Handled ex");
+                    }
+                    
+                }
+                Thread.Sleep(3000);
+            }
+        }
+
+        void AddNewItems()
+        {
+            while (true)
+            {
+                if (doNotSell)
+                {
+                    doNotSell = false;
+                    Thread.Sleep(500000);
+                }
+                else if (toBeSold.Count == 0)
+                {
+                    try
+                    {
+                        Inventory inventory = Protocol.GetSteamInventory();
+                        foreach (Inventory.SteamItem item in inventory.content)
+                        {
+                            Console.WriteLine(item.i_market_name + " is going to be sold.");
+                            toBeSold.Enqueue(item);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+
+                }
+                Thread.Sleep(500000);
+            }
+        }
+        
+        void SellFromQueue()
+        {
+            while (true)
+            {
+                if (toBeSold.Count != 0)
+                {
+                    Inventory.SteamItem item = toBeSold.Dequeue();
+                    if (dataBase.ContainsKey(item.i_market_name))
+                    {
+                        try
+                        {
+                            Protocol.Sell(item.i_classid, item.i_instanceid, dataBase[item.i_market_name].median);
+                        }
+                        catch(Exception ex)
+                        {
+
+                        }
+                    }
+                }
+                Thread.Sleep(2000);
+            }
+        }
+
+        void SaveDataBaseCycle()
+        {
+            while (true)
+            {
+                if (SaveDataBase())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Saved new DB");
+                    Console.WriteLine(dataBase.Count);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Couldn\'t save DB");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Thread.Sleep(6000);
+            }
+        }
+
+        public bool ReadFile(string path)
+        {
+            if (!File.Exists(path))
+                return false;
+            dataBase = BinarySerialization.ReadFromBinaryFile<Dictionary<string,SalesHistory>> (path);
+            return true;
+        }
+
+        public void LoadDataBase()
+        {
+            if (!File.Exists(DATABASEPATH))
+            {
+                Console.WriteLine("No database found, creating empty DB.");
+            }
+            else { 
+                dataBase = BinarySerialization.ReadFromBinaryFile<Dictionary<string, SalesHistory>>(DATABASEPATH);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Loaded new DB. Total item count: " + dataBase.Count);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public bool SaveDataBase()
+        {
+            try
+            {
+                if (File.Exists(DATABASEPATH))
+                {
+                    if (File.Exists(DATABASETEMPPATH))
+                        File.Delete(DATABASETEMPPATH);
+                    File.Copy(DATABASEPATH, DATABASETEMPPATH);
+                }
+                BinarySerialization.WriteToBinaryFile(DATABASEPATH, dataBase);
+                File.Delete(DATABASETEMPPATH);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not save DB, check whether DB name is correct (\'database.txt\'). Maybe this file is write-protected?:");
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        [Serializable]
+        public class SalesHistory
+        {
+            public List<HistoryItem> sales = new List<HistoryItem>();
+            public int median;
+            public int cnt = 0;
+
+            public SalesHistory(HistoryItem item)
+            {
+                cnt = 1;
+                sales.Add(item);
+            }
+        }
+
+        public void ProcessItem(HistoryItem item)
+        {
+            //Console.WriteLine(item.i_market_name);
+            SalesHistory salesHistory;
+            if (dataBase.ContainsKey(item.i_market_name))
+            {
+                salesHistory = dataBase[item.i_market_name];
+                if (dataBase[item.i_market_name].cnt == MAXSIZE)
+                    dataBase[item.i_market_name].sales.RemoveAt(0);
+                else
+                    dataBase[item.i_market_name].cnt++;
+                dataBase[item.i_market_name].sales.Add(item);
+            }
+            else
+            {
+                salesHistory = new SalesHistory(item);
+                dataBase.Add(item.i_market_name, salesHistory);
+            }
+
+            int[] a = new int[salesHistory.cnt];
+            for (int i = 0; i < salesHistory.cnt; i++)
+                a[i] = salesHistory.sales[i].price;
+            Array.Sort(a);
+            dataBase[item.i_market_name].median = a[(int)(salesHistory.cnt * 0.5)];
+
+            if (salesHistory.cnt >= MINSIZE)
+            {
+                needOrder.Enqueue(item);
+            }
+        }
+
+        public bool WantToBuy(NewItem item)
+        {
+            if (!dataBase.ContainsKey(item.i_market_name))
+                return false;
+            SalesHistory salesHistory = dataBase[item.i_market_name];
+            HistoryItem oldest = (HistoryItem)salesHistory.sales[0];
+            if (item.ui_price < 40000 && salesHistory.cnt >= MINSIZE && item.ui_price < 0.8 * salesHistory.median && salesHistory.median - item.ui_price > 600)
+            {//TODO какое-то условие на время
+                Console.WriteLine("Going to buy " + item.i_market_name + ". Expected profit " +  (salesHistory.median - item.ui_price));
+                return true;
+            }
+            return false;
+        }
+
+        public bool doNotSell = false; // True when we don`t want to sell.  
+        public Dota2Market Protocol;
+
+        private const int MAXSIZE = 500;
+        private const int MINSIZE = 80;
+        private const string DATABASEPATH = "database.txt";
+        private const string DATABASETEMPPATH = "databaseTemp.txt";
+        private Queue<Inventory.SteamItem> toBeSold = new Queue<Inventory.SteamItem>();
+        private Queue<HistoryItem> needOrder = new Queue<HistoryItem>();
+        private Dictionary<string, SalesHistory> dataBase = new Dictionary<string, SalesHistory>();
+        
+    }
+}
---
+++ MarketBot/DotaProtocol.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using SteamBot.SteamGroups;
+using SteamKit2;
+using WebSocket4Net;
+using SteamTrade;
+using SteamKit2.Internal;
+using SteamTrade.TradeOffer;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+
+namespace NDota2Market
+{
+    public class Dota2Market
+    {
+#if DEBUG
+        public int totalwasted = 0;
+#endif
+        private SteamBot.Bot Parent;
+        public Logic Logic;
+        string Api = "rQrm3yrEI48044Q0jCv7l3M7KMo1Cjn";
+
+        private string ExecuteApiRequest(string url)
+        {
+            using (WebClient myWebClient = new WebClient())
+            {
+                NameValueCollection myQueryStringCollection = new NameValueCollection();
+                myQueryStringCollection.Add("q", "");
+                myWebClient.QueryString = myQueryStringCollection;
+                return myWebClient.DownloadString("https://market.dota2.net" + url);
+            }
+        }
+
+        public Dota2Market()
+        {
+
+        }
+        bool died = true;
+        WebSocket socket = new WebSocket("wss://wsn.dota2.net/wsn/");
+        public Dota2Market(SteamBot.Bot p)
+        {
+            Parent = p;
+            Thread starter = new Thread(new ThreadStart(StartUp));
+            starter.Start();
+        }
+        
+        private void StartUp()
+        {
+            while (Logic == null)
+                Thread.Sleep(10);
+            socket.Opened += Open;
+            socket.Closed += Error;
+            socket.MessageReceived += Msg;
+            socket.Open();
+        }
+
+        public readonly string[] search = { "<div class=\\\"price\\\"", "<div class=\\\"name\\\"" };
+
+        public class Dummy
+        {
+            public string s;
+        }
+
+        string parse(string s)
+        {
+            var sb = new StringBuilder();
+            sb.Append("{\"s\":\"" + s + "\"}");
+            string t = sb.ToString();
+            var dummy = JsonConvert.DeserializeObject<Dummy>(t);
+            return dummy.s;
+        }
+
+        static string DecodeEncodedNonAsciiCharacters(string value)
+        {
+            return Regex.Replace(
+                value,
+                @"\\u(?<Value>[a-zA-Z0-9]{4})",
+                m => {
+                    return ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString();
+                });
+        }
+
+        void Msg(object sender, MessageReceivedEventArgs e)
+        {
+            if (e.Message == "pong")
+                return;
+            var message = e.Message;
+            Message x = JsonConvert.DeserializeObject<Message>(message);
+            //Console.WriteLine(x.type);
+            switch (x.type)
+            {
+                case "newitems_cs":
+                    {
+                        NewItem newItem = JsonConvert.DeserializeObject<NewItem>(x.data);
+                        newItem.ui_price = newItem.ui_price * 100 + 0.5f;
+                        if (Logic.WantToBuy(newItem))
+                        {
+                            Buy(newItem);
+                            Console.WriteLine(newItem.i_market_name + " " + newItem.ui_price);
+                        }
+                        break;
+                    }
+                case "history_cs":
+                    {
+                        try
+                        {
+                            char[] trimming = { '[', ']' };
+                            x.data = DecodeEncodedNonAsciiCharacters(x.data);
+                            //Console.WriteLine(x.data);
+                            x.data = x.data.Replace("\\", "").Replace("\"", "").Trim(trimming);
+                            string[] arr = x.data.Split(',');
+                            HistoryItem historyItem = new HistoryItem();
+                            if (arr.Length == 7)
+                            {
+                                historyItem.i_classid = arr[0];
+                                historyItem.i_instanceid = arr[1];
+                                historyItem.i_market_hash_name = arr[2];
+                                historyItem.timesold = arr[3];
+                                historyItem.price = Int32.Parse(arr[4]);
+                                historyItem.i_market_name = arr[5];
+                            }
+                            else
+                            {
+                                throw new Exception(x.data + " is not a valid history item.");
+                            }
+                            Logic.ProcessItem(historyItem);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(ex.Message);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    }
+                default:
+                    Console.WriteLine(x.type);
+                    x.data = DecodeEncodedNonAsciiCharacters(x.data);
+                    Console.WriteLine(x.data);
+                    break;
+            }
+        }
+
+        void pinger()
+        {
+            while (!died)
+            {
+                socket.Send("ping");
+                Thread.Sleep(30000);
+            }
+        }
+
+        bool TakeItems()
+        {
+            Console.WriteLine("Taking items");
+            JObject json = JObject.Parse(ExecuteApiRequest("/api/ItemRequest/in/1/?key=" + Api));
+            if (json["success"] == null)
+                return false;
+            else if ((bool)json["success"])
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        bool GiveItems(string botID)
+        {
+            Console.WriteLine("Giving items");
+            JObject json = JObject.Parse(ExecuteApiRequest("/api/ItemRequest/out/" + botID + "/?key=" + Api));
+            if (json["success"] == null)
+                return false;
+            else if ((bool)json["success"])
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        void HandleTrades()
+        {
+            while (!died)
+            {
+                try
+                {
+                    TMTrade[] arr = GetTradeList();
+                    UpdateInventory();
+                    bool had = false;
+                    bool gone = false;
+                    for (int i = 0; i < arr.Length; ++i)
+                    {
+                        if (arr[i].ui_status == "4")
+                        {
+                            UpdateInventory();
+                            GiveItems(arr[i].ui_bid);
+                            gone = true;
+                            Logic.doNotSell = true;
+                            break;
+                        }
+                        had |= arr[i].ui_status == "2";
+                    }
+                    if (had && !gone)
+                    {
+                        UpdateInventory();
+                        TakeItems();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+                //once per 30 seconds we check trade list
+                Thread.Sleep(30000);
+            }
+        }
+
+        void Subscribe()
+        {
+            socket.Send("newitems_cs");
+            socket.Send("history_cs");
+        }
+
+        void Auth()
+        {
+            Auth q = JsonConvert.DeserializeObject<Auth>(ExecuteApiRequest("/api/GetWSAuth/?key=" + Api));
+            socket.Send(q.wsAuth);
+            Subscribe();
+        }
+
+        void Open(object sender, EventArgs e)
+        {
+            died = false;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Connection opened!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Auth();
+            Thread ping = new Thread(new ThreadStart(pinger));
+            ping.Start();
+            Thread tradeHandler = new Thread(new ThreadStart(HandleTrades));
+            tradeHandler.Start();
+        }
+
+        void Error(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error");
+            died = true;
+            ReOpen();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        void ReOpen()
+        {
+            for (int i = 0; died && i < 10; ++i)
+            {
+                socket = new WebSocket("wss://wsn.dota2.net/wsn/");
+                socket.Opened += Open;
+                socket.Closed += Error;
+                socket.MessageReceived += Msg;
+                socket.Open();
+                Thread.Sleep(5000);
+                Console.WriteLine("Trying to reconnect for the %d-th time", i + 1);
+            }
+        }
+
+
+        public bool Buy(NewItem item)
+        {
+#if DEBUG
+            totalwasted += (int)item.ui_price;
+            Console.WriteLine("Purchased an item for {0}, total wasted {1}", ((int)item.ui_price + .0) / 100, (totalwasted + .0) / 100);
+            return true;
+#else
+            string a = ExecuteApiRequest("/api/Buy/" + item.i_classid + "_" + item.i_instanceid + "/" + ((int)item.ui_price).ToString() + "/?key=" + Api);
+            JObject parsed = JObject.Parse(a);
+            foreach (var pair in parsed)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+            if (parsed["result"] == null)
+                return false;
+            else if ((string)parsed["result"] == "ok")
+                return true;
+            else
+                return false;
+#endif
+        }
+
+        //Interface starts here:
+        [System.Obsolete("Specify item, it will parce it by itself.")]
+        public bool Buy(string ClasssId, string InstanceId, int price)
+        {
+#if DEBUG
+            totalwasted += price;
+            Console.WriteLine("Purchased an item for {0}, total wasted {1}", (price + .0) / 100, (totalwasted + .0) / 100);
+            return true;
+#else
+            string a = ExecuteApiRequest("/api/Buy/" + ClasssId + "_" + InstanceId + "/" + price.ToString() + "/?key=" + Api);
+            JObject parsed = JObject.Parse(a);
+            foreach (var pair in parsed)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+            if (parsed["result"] == null)
+                return false;
+            else if ((string)parsed["result"] == "ok")
+                return true;
+            else
+                return false;
+#endif
+        }
+
+        public bool Sell(string ClasssId, string InstanceId, int price)
+        {
+#if DEBUG //sorry nothing is implemented there, I don't really know what to write as debug
+            return false;
+
+#else
+            string a = ExecuteApiRequest("/api/SetPrice/new_" + ClasssId + "_" + InstanceId + "/" + price.ToString() + "/?key=" + Api);
+            JObject parsed = JObject.Parse(a);
+            foreach (var pair in parsed)
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            if (parsed["result"] == null)
+                return false;
+            else if ((string)parsed["result"] == "ok")
+                return true;
+            else
+                return false;
+#endif
+        }
+
+        public Inventory GetSteamInventory()
+        {
+            string a = ExecuteApiRequest("/api/GetInv/?key=" + Api);
+            JObject json = JObject.Parse(a);
+            Inventory inventory = new Inventory();
+            inventory.content = new List<Inventory.SteamItem>();
+            if (json["ok"] != null && (bool)json["ok"] == false)
+                return inventory;
+            inventory.content = json["data"].ToObject<List<Inventory.SteamItem>>();
+            return inventory;
+        }
+
+        public bool SetOrder(string classid, string instanceid, int price)
+        {
+#if DEBUG   
+            return false;
+#else
+            string a = ExecuteApiRequest("/api/ProcessOrder/" + classid + "/" + instanceid + "/" + price.ToString() + "/?key=" + Api);
+            JObject json = JObject.Parse(a);
+            //foreach (var thing in json)
+            //..Console.WriteLine("{0}: {1}", thing.Key, thing.Value);
+            if (json["success"] == null)
+                return false;
+            else if ((bool)json["success"])
+                return true;
+            else
+                 return false;
+#endif
+        }
+
+        bool UpdateInventory()
+        {
+            try
+            {
+                string a = ExecuteApiRequest("/api/UpdateInventory/?key=" + Api);
+                JObject json = JObject.Parse(a);
+                //foreach (var thing in json)
+                //    Console.WriteLine("{0}: {1}", thing.Key, thing.Value);
+                //cout<<;
+                if (json["success"] == null)
+                    return false;
+                else if ((bool)json["success"])
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public int getBestOrder(string classid, string instanceid)
+        {
+            try
+            {
+                string a = ExecuteApiRequest("/api/ItemInfo/" + classid + "_" + instanceid + "/ru/?key=" + Api);
+                JObject x = JObject.Parse(a);
+                JArray thing = (JArray)x["buy_offers"];
+                if (thing == null || thing.Count == 0)
+                    return 49;
+                else
+                    return int.Parse(((string)thing[0]["o_price"]));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        TMTrade[] GetTradeList()
+        {
+            try
+            {
+                string a = ExecuteApiRequest("/api/Trades/?key=" + Api);
+                JArray json = JArray.Parse(a);
+                TMTrade[] arr = new TMTrade[json.Count];
+                int iter = 0;
+                foreach (var thing in json)
+                {
+                    //Console.WriteLine("{0}", thing);
+                    TMTrade xx = JsonConvert.DeserializeObject<TMTrade>(thing.ToString());
+                    arr[iter++] = xx;
+                }
+                return arr;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
