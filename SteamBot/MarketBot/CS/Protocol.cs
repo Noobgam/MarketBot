c2d11cdc08d4012aa2717a9fb5dbb34aa854e6cf
AndreySmirdin <andrey.smirdin@mail.ru>
2018-01-23 15:50:40 +0300

    code style is great again!

--- SteamBot/MarketBot/CS/Protocol.cs
+++ SteamBot/MarketBot/CS/Protocol.cs
@@ -20,10 +20,8 @@ using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using System.Collections.Specialized;
 
-namespace CSGOTM
-{
-    public class Protocol
-    {
+namespace CSGOTM {
+    public class Protocol {
 #if DEBUG
         public int totalwasted = 0;
 #endif
@@ -32,27 +30,24 @@ namespace CSGOTM
         public Logic Logic;
         string Api = "rQrm3yrEI48044Q0jCv7l3M7KMo1Cjn";
 
-        private string ExecuteApiRequest(string url)
-        {
-            using (WebClient myWebClient = new WebClient())
-            {
+        private string ExecuteApiRequest(string url) {
+            using (WebClient myWebClient = new WebClient()) {
                 NameValueCollection myQueryStringCollection = new NameValueCollection();
                 myQueryStringCollection.Add("q", "");
                 myWebClient.QueryString = myQueryStringCollection;
                 return myWebClient.DownloadString("https://market.csgo.com" + url);
             }
         }
-        
+
         bool died = true;
         WebSocket socket = new WebSocket("wss://wsn.dota2.net/wsn/");
-        public Protocol()
-        {
+
+        public Protocol() {
             Thread starter = new Thread(new ThreadStart(StartUp));
             starter.Start();
         }
-        
-        private void StartUp()
-        {
+
+        private void StartUp() {
             while (Logic == null)
                 Thread.Sleep(10);
             QueuedOffers = new Queue<TradeOffer>();
@@ -62,15 +57,13 @@ namespace CSGOTM
             socket.Open();
         }
 
-        public readonly string[] search = { "<div class=\\\"price\\\"", "<div class=\\\"name\\\"" };
+        public readonly string[] search = {"<div class=\\\"price\\\"", "<div class=\\\"name\\\""};
 
-        public class Dummy
-        {
+        public class Dummy {
             public string s;
         }
 
-        string parse(string s)
-        {
+        string parse(string s) {
             var sb = new StringBuilder();
             sb.Append("{\"s\":\"" + s + "\"}");
             string t = sb.ToString();
@@ -78,47 +71,40 @@ namespace CSGOTM
             return dummy.s;
         }
 
-        static string DecodeEncodedNonAsciiCharacters(string value)
-        {
+        static string DecodeEncodedNonAsciiCharacters(string value) {
             return Regex.Replace(
                 value,
                 @"\\u(?<Value>[a-zA-Z0-9]{4})",
-                m => {
-                    return ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString();
-                });
+                m => { return ((char) int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString(); });
         }
 
-        void Msg(object sender, MessageReceivedEventArgs e)
-        {
+        void Msg(object sender, MessageReceivedEventArgs e) {
             if (e.Message == "pong")
                 return;
             var message = e.Message;
             Message x = JsonConvert.DeserializeObject<Message>(message);
             //Console.WriteLine(x.type);
-            switch (x.type)
-            {
+            switch (x.type) {
                 case "newitems_go":
                     NewItem newItem = JsonConvert.DeserializeObject<NewItem>(x.data);
                     //getBestOrder(newItem.i_classid, newItem.i_instanceid);
                     newItem.ui_price = newItem.ui_price * 100 + 0.5f;
-                    if (Logic.WantToBuy(newItem))
-                    {
+                    if (Logic.WantToBuy(newItem)) {
                         if (Buy(newItem))
                             Log.Success("Purchased: " + newItem.i_market_name + " " + newItem.ui_price);
                         else
                             Log.Warn("Couldn\'t purchase " + newItem.i_market_name + " " + newItem.ui_price);
                     }
+
                     break;
                 case "history_go":
-                    try
-                    {
-                        char[] trimming = { '[', ']' };
+                    try {
+                        char[] trimming = {'[', ']'};
                         x.data = DecodeEncodedNonAsciiCharacters(x.data);
                         x.data = x.data.Replace("\\", "").Replace("\"", "").Trim(trimming);
                         string[] arr = x.data.Split(',');
                         HistoryItem historyItem = new HistoryItem();
-                        if (arr.Length == 7)
-                        {
+                        if (arr.Length == 7) {
                             historyItem.i_classid = arr[0];
                             historyItem.i_instanceid = arr[1];
                             historyItem.i_market_hash_name = arr[2];
@@ -126,8 +112,7 @@ namespace CSGOTM
                             historyItem.price = Int32.Parse(arr[4]);
                             historyItem.i_market_name = arr[5];
                         }
-                        else if (arr.Length == 8)
-                        {
+                        else if (arr.Length == 8) {
                             historyItem.i_classid = arr[0];
                             historyItem.i_instanceid = arr[1];
                             historyItem.i_market_hash_name = arr[2] + arr[3];
@@ -135,16 +120,16 @@ namespace CSGOTM
                             historyItem.price = Int32.Parse(arr[5]);
                             historyItem.i_market_name = arr[6];
                         }
-                        else
-                        {
+                        else {
                             throw new Exception(x.data + " is not a valid history item.");
                         }
+
                         Logic.ProcessItem(historyItem);
                     }
-                    catch (Exception ex)
-                    {
+                    catch (Exception ex) {
                         Log.Error(ex.Message);
                     }
+
                     break;
 
                 case "invcache_go":
@@ -159,109 +144,93 @@ namespace CSGOTM
                     //Console.WriteLine(x.type);
                     x.data = DecodeEncodedNonAsciiCharacters(x.data);
                     Log.Info(x.data);
-                    try
-                    {
+                    try {
                         JObject json = JObject.Parse(x.data);
-                        if ((int)json["status"] == 5)
+                        if ((int) json["status"] == 5)
                             Logic.doNotSell = true;
                     }
-                    catch (Exception ex)
-                    {
-
+                    catch (Exception ex) {
                     }
+
                     break;
             }
         }
 
-        void pinger()
-        {
-            while (!died)
-            {
+        void pinger() {
+            while (!died) {
                 socket.Send("ping");
                 Thread.Sleep(30000);
             }
         }
 
-        bool TakeItems()
-        {
+        bool TakeItems() {
             Log.Info("Taking items");
             JObject json = JObject.Parse(ExecuteApiRequest("/api/ItemRequest/in/1/?key=" + Api));
             if (json["success"] == null)
                 return false;
-            else if ((bool)json["success"])
-            {
+            else if ((bool) json["success"]) {
                 return true;
             }
             else
                 return false;
         }
 
-        bool GiveItems(string botID)
-        {
+        bool GiveItems(string botID) {
             Log.Info("Giving items");
             JObject json = JObject.Parse(ExecuteApiRequest("/api/ItemRequest/out/" + botID + "/?key=" + Api));
             if (json["success"] == null)
                 return false;
-            else if ((bool)json["success"])
-            {
+            else if ((bool) json["success"]) {
                 return true;
             }
             else
                 return false;
         }
 
-        void HandleTrades()
-        {
-            while (!died)
-            {
-                try
-                {
+        void HandleTrades() {
+            while (!died) {
+                try {
                     TMTrade[] arr = GetTradeList();
                     UpdateInventory();
                     bool had = false;
                     bool gone = false;
-                    for (int i = 0; i < arr.Length; ++i)
-                    {
-                        if (arr[i].ui_status == "4")
-                        {
+                    for (int i = 0; i < arr.Length; ++i) {
+                        if (arr[i].ui_status == "4") {
                             UpdateInventory();
                             GiveItems(arr[i].ui_bid);
                             gone = true;
                             Logic.doNotSell = true;
                             break;
                         }
+
                         had |= arr[i].ui_status == "2";
                     }
-                    if (had && !gone)
-                    {
+
+                    if (had && !gone) {
                         UpdateInventory();
                         TakeItems();
                     }
                 }
-                catch (Exception ex)
-                {
-
+                catch (Exception ex) {
                 }
+
                 //once per 30 seconds we check trade list
                 Thread.Sleep(30000);
             }
         }
 
-        void Subscribe()
-        {
+        void Subscribe() {
             socket.Send("newitems_go");
             socket.Send("history_go");
         }
 
-        void Auth()
-        {
+        void Auth() {
             Auth q = JsonConvert.DeserializeObject<Auth>(ExecuteApiRequest("/api/GetWSAuth/?key=" + Api));
             socket.Send(q.wsAuth);
             Subscribe();
         }
 
-        void Open(object sender, EventArgs e)
-        {
+        void Open(object sender, EventArgs e) {
             died = false;
             Log.Success("Connection opened!");
             Auth();
@@ -272,17 +241,14 @@ namespace CSGOTM
             //andrew is gay
         }
 
-        void Error(object sender, EventArgs e)
-        {
+        void Error(object sender, EventArgs e) {
             Log.Error("Connection error");
             died = true;
             ReOpen();
         }
 
-        void ReOpen()
-        {
-            for (int i = 0; died && i < 10; ++i)
-            {
+        void ReOpen() {
+            for (int i = 0; died && i < 10; ++i) {
                 socket = new WebSocket("wss://wsn.dota2.net/wsn/");
                 socket.Opened += Open;
                 socket.Closed += Error;
@@ -294,14 +260,15 @@ namespace CSGOTM
         }
 
 
-        public bool Buy(NewItem item)
-        {
+        public bool Buy(NewItem item) {
 #if DEBUG
-            totalwasted += (int)item.ui_price;
-            Log.Debug("Purchased an item for {0}, total wasted {1}", ((int)item.ui_price + .0) / 100, (totalwasted + .0) / 100);
+            totalwasted += (int) item.ui_price;
+            Log.Debug("Purchased an item for {0}, total wasted {1}", ((int) item.ui_price + .0) / 100,
+                (totalwasted + .0) / 100);
             return true;
 #else
-            string a = ExecuteApiRequest("/api/Buy/" + item.i_classid + "_" + item.i_instanceid + "/" + ((int)item.ui_price).ToString() + "/?key=" + Api);
+            string a =
+ ExecuteApiRequest("/api/Buy/" + item.i_classid + "_" + item.i_instanceid + "/" + ((int)item.ui_price).ToString() + "/?key=" + Api);
             JObject parsed = JObject.Parse(a);
             //foreach (var pair in parsed)
             //{
@@ -318,14 +285,14 @@ namespace CSGOTM
 
         //Interface starts here:
         [System.Obsolete("Specify item, it will parce it by itself.")]
-        public bool Buy(string ClasssId, string InstanceId, int price)
-        {
+        public bool Buy(string ClasssId, string InstanceId, int price) {
 #if DEBUG
             totalwasted += price;
             Log.Success("Purchased an item for {0}, total wasted {1}", (price + .0) / 100, (totalwasted + .0) / 100);
             return true;
 #else
-            string a = ExecuteApiRequest("/api/Buy/" + ClasssId + "_" + InstanceId + "/" + price.ToString() + "/?key=" + Api);
+            string a =
+ ExecuteApiRequest("/api/Buy/" + ClasssId + "_" + InstanceId + "/" + price.ToString() + "/?key=" + Api);
             JObject parsed = JObject.Parse(a);
             foreach (var pair in parsed)
             {
@@ -340,13 +307,13 @@ namespace CSGOTM
 #endif
         }
 
-        public bool Sell(string ClasssId, string InstanceId, int price)
-        {
+        public bool Sell(string ClasssId, string InstanceId, int price) {
 #if DEBUG //sorry nothing is implemented there, I don't really know what to write as debug
             return false;
 
 #else
-            string a = ExecuteApiRequest("/api/SetPrice/new_" + ClasssId + "_" + InstanceId + "/" + price.ToString() + "/?key=" + Api);
+            string a =
+ ExecuteApiRequest("/api/SetPrice/new_" + ClasssId + "_" + InstanceId + "/" + price.ToString() + "/?key=" + Api);
             JObject parsed = JObject.Parse(a);
             foreach (var pair in parsed)
                 Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
@@ -359,24 +326,23 @@ namespace CSGOTM
 #endif
         }
 
-        public Inventory GetSteamInventory()
-        {
+        public Inventory GetSteamInventory() {
             string a = ExecuteApiRequest("/api/GetInv/?key=" + Api);
             JObject json = JObject.Parse(a);
             Inventory inventory = new Inventory();
             inventory.content = new List<Inventory.SteamItem>();
-            if (json["ok"] != null && (bool)json["ok"] == false)
+            if (json["ok"] != null && (bool) json["ok"] == false)
                 return inventory;
             inventory.content = json["data"].ToObject<List<Inventory.SteamItem>>();
             return inventory;
         }
 
-        public bool SetOrder(string classid, string instanceid, int price)
-        {
-#if DEBUG   
+        public bool SetOrder(string classid, string instanceid, int price) {
+#if DEBUG
             return false;
 #else
-            string a = ExecuteApiRequest("/api/ProcessOrder/" + classid + "/" + instanceid + "/" + price.ToString() + "/?key=" + Api);
+            string a =
+ ExecuteApiRequest("/api/ProcessOrder/" + classid + "/" + instanceid + "/" + price.ToString() + "/?key=" + Api);
             JObject json = JObject.Parse(a);
             //foreach (var thing in json)
             //..Console.WriteLine("{0}: {1}", thing.Key, thing.Value);
@@ -389,10 +355,8 @@ namespace CSGOTM
 #endif
         }
 
-        bool UpdateInventory()
-        {
-            try
-            {
+        bool UpdateInventory() {
+            try {
                 string a = ExecuteApiRequest("/api/UpdateInventory/?key=" + Api);
                 JObject json = JObject.Parse(a);
                 //foreach (var thing in json)
@@ -400,53 +364,46 @@ namespace CSGOTM
                 //cout<<;
                 if (json["success"] == null)
                     return false;
-                else if ((bool)json["success"])
+                else if ((bool) json["success"])
                     return true;
                 else
                     return false;
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 return false;
             }
         }
 
-        public int getBestOrder(string classid, string instanceid)
-        {
-            try
-            {
+        public int getBestOrder(string classid, string instanceid) {
+            try {
                 string a = ExecuteApiRequest("/api/ItemInfo/" + classid + "_" + instanceid + "/ru/?key=" + Api);
                 JObject x = JObject.Parse(a);
-                JArray thing = (JArray)x["buy_offers"];
+                JArray thing = (JArray) x["buy_offers"];
                 if (thing == null || thing.Count == 0)
                     return 49;
                 else
-                    return int.Parse(((string)thing[0]["o_price"]));
+                    return int.Parse(((string) thing[0]["o_price"]));
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 throw ex;
             }
         }
 
-        TMTrade[] GetTradeList()
-        {
-            try
-            {
+        TMTrade[] GetTradeList() {
+            try {
                 string a = ExecuteApiRequest("/api/Trades/?key=" + Api);
                 JArray json = JArray.Parse(a);
                 TMTrade[] arr = new TMTrade[json.Count];
                 int iter = 0;
-                foreach (var thing in json)
-                {
+                foreach (var thing in json) {
                     //Console.WriteLine("{0}", thing);
                     TMTrade xx = JsonConvert.DeserializeObject<TMTrade>(thing.ToString());
                     arr[iter++] = xx;
                 }
+
                 return arr;
             }
-            catch
-            {
+            catch {
                 return null;
             }
         }
