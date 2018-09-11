bc63dc560afa06b2dd626000cb3b3c8049a337b0
Noobgam <shessmaster12@gmail.com>
2018-09-11 22:20:00 +0300

    benchmarking

--- SteamBot/MarketBot/Utility/Request.cs
+++ SteamBot/MarketBot/Utility/Request.cs
@@ -1,20 +1,24 @@
 ﻿using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Utility {
     public static class Request {
 
-        public static string Get(string uri) {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.Proxy = null;
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+        public static HttpClient httpClient = new HttpClient();
 
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream)) {
-                return reader.ReadToEnd();
-            }
+        public static async Task<string> NewGet(string uri) {
+            
+            return await (await httpClient.GetAsync(uri)).Content.ReadAsStringAsync();
+        }
+
+        public static string Get(string uri)
+        {
+            var temp = NewGet(uri);
+            temp.Wait();
+            return temp.Result;
         }
 
         public static string Post(string uri, string data, string contentType = "application/x-www-form-urlencoded", string method = "POST") {
