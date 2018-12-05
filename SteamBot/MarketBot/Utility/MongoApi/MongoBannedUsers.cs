71ca4deb77957e711f60f294d30f8b142b1d3acc
Noobgam <shessmaster12@gmail.com>
2018-12-05 23:28:41 +0300

    this is no longer necessary

--- SteamBot/MarketBot/Utility/MongoApi/MongoBannedUsers.cs
+++ SteamBot/MarketBot/Utility/MongoApi/MongoBannedUsers.cs
@@ -9,20 +9,6 @@ using System.Threading.Tasks;
 
 namespace SteamBot.MarketBot.Utility.MongoApi {
     public class MongoBannedUsers : GenericMongoDB<BannedUser> {
-        public MongoBannedUsers() {
-            Add(76561198379677339L
-                ,76561198328630783L
-                ,76561198321472965L
-                ,76561198408228242L
-                ,76561198033167623L
-                ,76561198857835986L
-                ,76561198857940860L
-                ,76561198328630783L
-                ,76561198356087536L
-                ,76561198309616729L
-                ,76561198316325564L
-                ,76561198027819122L);            
-        }
 
         public override string GetCollectionName() {
             return "banned_users";
