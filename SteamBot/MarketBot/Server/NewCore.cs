76822d153a91e9429d61e7ebf38a059bd8806ba6
Noobgam <shessmaster12@gmail.com>
2019-02-18 21:38:30 +0300

    change vk app

--- SteamBot/MarketBot/Server/NewCore.cs
+++ SteamBot/MarketBot/Server/NewCore.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Utility.VK;
 
 namespace Server
 {
@@ -37,6 +38,7 @@ namespace Server
             logger.Info("Initializing core");
             InitializeEndpoints();
             httpListener.Start();
+            VK.Init();
             logger.Success("Started serving");
             tokenSource = new CancellationTokenSource();
             ct = tokenSource.Token;
